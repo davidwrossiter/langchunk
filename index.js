601f1a819a37fe542330e324fc5ae88f88cace10
davidwrossiter <davewrossiter@gmail.com>
2024-06-18 11:27:22 +0100

    minor bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+#!/usr/bin/env node
 const esprima = require("esprima");
 const estraverse = require("estraverse");
+
+const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
+const path = require("path");
+
+program
+  .version("1.0.0")
+  .arguments("<directory>")
+  .description("Chunk JavaScript files in a directory")
+  .action((directory) => {
+    console.log(`Chunking files in directory ${directory}`);
+    processDirectory(directory);
+  });
+
+program.parse(process.argv);
+
+function processDirectory(dir) {
+  fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory: ${err.message}`);
+      return;
+    }
+
+    files.forEach((file) => {
+      const fullPath = path.join(dir, file.name);
+
+      if (file.isDirectory()) {
+        processDirectory(fullPath);
+      } else if (
+        file.isFile() &&
+        (file.name.endsWith(".js") || file.name.endsWith(".jsx"))
+      ) {
+        console.log(chunkFile(fullPath));
+      }
+    });
+  });
+}
 
 const code = `function addMetadata(chunks) {
   return chunks.map((chunk, index) => ({
@@ -15,10 +54,24 @@ const chunksWithMetadata = addMetadata(finalChunks);
 console.log(chunksWithMetadata);
 `;
 
-const ast = esprima.parseScript(code, { loc: true });
-fileName = "chunker.js";
+function chunkFile(filePath) {
+  // Read file contents
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${filePath}, ${err.message}`);
+      return;
+    }
 
-function extractCodeChunks(ast, sourceCode, fileName) {
+    // Chunking logic goes here
+    const chunks = extractCodeChunks(data);
+
+    // Example: Print chunks to console (replace with actual storage logic)
+    console.log(`File: ${filePath}, Chunks: ${chunks.length}`);
+  });
+}
+
+function extractCodeChunks(sourceCode) {
+  const ast = esprima.parseScript(code, { loc: true });
   const chunks = [];
   estraverse.traverse(ast, {
     enter: function (node, parent) {
@@ -31,14 +84,14 @@ function extractCodeChunks(ast, sourceCode, fileName) {
         const start = node.loc.start.line - 1;
         const end = node.loc.end.line;
         const chunk = sourceCode.split("\n").slice(start, end).join("\n");
-
+        console.log("Chunk: ", chunk);
         chunks.push(chunk);
       }
     },
   });
   return chunks;
 }
-const codeChunks = extractCodeChunks(ast, code);
+// const codeChunks = extractCodeChunks(ast, code);
 // console.log(codeChunks);
 
 const MAX_CHUNK_SIZE = 1000;
@@ -56,8 +109,7 @@ function adjustChunkSize(chunks) {
   return adjustedChunks;
 }
 
-const finalChunks = adjustChunkSize(codeChunks);
-// console.log(finalChunks);
+// const finalChunks = adjustChunkSize(codeChunks);
 
 function determineType(chunk) {
   // Implement this based on your needs
@@ -75,8 +127,8 @@ function addMetadata(chunks) {
   }));
 }
 
-const chunksWithMetadata = addMetadata(finalChunks);
-console.log(chunksWithMetadata);
+// const chunksWithMetadata = addMetadata(finalChunks);
+// console.log(chunksWithMetadata);
 
 // const vectorDatabase = require("vector-database-client");
 
